742823e317022a1ecb543bb63dc5ce3fe911a71c
karanberry237-bit <karanberry237@gmail.com>
2026-06-29 19:04:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1135,8 +1135,8 @@ let SHADOW_TRIGGERS = [
   "cosa is bad","cosa is trash","cosa sucks","cosa is stupid","cosa is dumb",
   "cosa is useless","hate cosa","cosa is terrible","down with clint",
   "clint is corrupt","clint doesn't deserve","clint is unfair","overthrow clint",
-  "clint should be removed","remove clint","clint abuse","don is bad","fuck clint","fuck you clint","fuck you cosa"
-
+  "clint should be removed","remove clint","clint abuse","don is bad","fuck clint","fuck you clint","fuck you cosa","Clanker"
+  
 ];
 
 // ── Fingerprint / Anti-Alt System ────────────────────────────────────────────
