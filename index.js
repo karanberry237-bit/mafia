ca9abab35bb53650c84aab2042115696c5c8dc65
karanberry237-bit <karanberry237@gmail.com>
2026-06-29 19:03:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1136,7 +1136,6 @@ let SHADOW_TRIGGERS = [
   "cosa is useless","hate cosa","cosa is terrible","down with clint",
   "clint is corrupt","clint doesn't deserve","clint is unfair","overthrow clint",
   "clint should be removed","remove clint","clint abuse","don is bad","fuck clint","fuck you clint","fuck you cosa"
-  "who made clint don?","Clanker",
 
 ];
 
